c80a944372907581948b2e7b081ab5c300f3391b
Maggie White <maggiewhite@google.com>
2018-10-10 17:16:55 -0700

    smb5-lib: add USB port temp to usb power_supply
    Bug: 116656846
    Test: cat /sys/class/power_supply/usb/temp
    Change-Id: I231a72ae614ff1e68be034264dfc41665ef3aebc
    Signed-off-by: Maggie White <maggiewhite@google.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/power/supply/qcom/smb5-lib.h
+++ drivers/power/supply/qcom/smb5-lib.h
@@ -18,6 +18,7 @@
 #include <linux/timer.h>
 #include <linux/interrupt.h>
 #include <linux/irqreturn.h>
+#include <linux/thermal.h>
 #include <linux/regulator/driver.h>
 #include <linux/regulator/consumer.h>
 #include <linux/extcon.h>
@@ -708,6 +709,10 @@ struct smb_charger {
 	int			usb_icl_delta_ua;
 	int			pulse_cnt;
 
+	/* USB port temperature */
+	const char		*usb_port_tz_name;
+	struct thermal_zone_device *usb_port_tz;
+
 	int			die_health;
 	int			connector_health;
 	/* raise qc3 vbus flag */
@@ -891,6 +896,8 @@ int smblib_get_prop_usb_voltage_now(struct smb_charger *chg,
 int smblib_get_prop_low_power(struct smb_charger *chg,
 				union power_supply_propval *val);
 int smblib_get_prop_usb_current_now(struct smb_charger *chg,
+				union power_supply_propval *val);
+int smblib_get_prop_usb_port_temp(struct smb_charger *chg,
 				union power_supply_propval *val);
 int smblib_get_prop_typec_cc_orientation(struct smb_charger *chg,
 				union power_supply_propval *val);
